d52f009ec2c3424e4e2a1ab25f5b2286e9928d7e
Bruce Sutherland <bsutherland@users.noreply.github.com>
2016-11-05 15:36:46 +0900

    Download postcode data from GitHub in production mode.

--- app/services/JPIService.scala
+++ app/services/JPIService.scala
@@ -1,12 +1,13 @@
 package services
 
-import play.api.Logger
-import play.api.Environment
+import play.Environment
+import play.api.{Play, Logger}
 import play.api.cache._
 
 import javax.inject._
 import java.io.{BufferedReader, InputStreamReader, File, FileInputStream}
 import java.util.zip.ZipInputStream
+import java.net.URL
 
 import com.github.tototoshi.csv._
 
@@ -21,7 +22,8 @@ class CacheJPIService @Inject() (
   cache: CacheApi, env: Environment
 ) extends JPIService  {
 
-  final val JPURI = "http://www.post.japanpost.jp/zipcode/dl/kogaki/zip/ken_all.zip"
+  // final val JPURI = "http://www.post.japanpost.jp/zipcode/dl/kogaki/zip/ken_all.zip"
+  final val JPURI = "https://github.com/bsutherland/jpi/raw/master/data/ken_all.zip"
   final val ZippedCSVPath = new File("data", "ken_all.zip").toString
   final val CSVEncoding = "SJIS"
 
@@ -39,8 +41,16 @@ class CacheJPIService @Inject() (
     - Insert models into cache, keyed by postal code
    */
   def buildCache() {
-    Logger.info(s"Initializing cache from $ZippedCSVPath.")
-    val inStream = new ZipInputStream(new FileInputStream(env.getFile(ZippedCSVPath)))
+    val inStream = new ZipInputStream(
+      if (!env.isProd) {
+        Logger.info(s"Initializing cache from $ZippedCSVPath.")
+        new FileInputStream(env.underlying.getFile(ZippedCSVPath))
+      } else {
+        Logger.info(s"Initializing cache from $JPURI.")
+        new URL(JPURI).openStream
+      }
+    )
+
     val csvFile = Option(inStream.getNextEntry)
     csvFile.foreach { _ =>
       val reader = CSVReader.open(new InputStreamReader(inStream, CSVEncoding))
